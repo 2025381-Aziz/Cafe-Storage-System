598c7b34a141784f7704e9cd37b96985bfd8721c
2025381-Aziz <2025381@student.cct.ie>
2026-04-19 18:58:26 +0100

    Added removeFood and peekFood methods

--- Storage.java
+++ Storage.java
@@ -8,13 +8,29 @@ class Storage {
     private final int MAX_CAPACITY = 8;
 
     public void addFood(FoodItem item) {
-
         if (queue.size() >= MAX_CAPACITY) {
             System.out.println("Storage is FULL (Max 8 items)");
             return;
         }
-
         queue.add(item);
         System.out.println("Food added successfully!");
+    }
+
+    public void removeFood() {
+        if (queue.isEmpty()) {
+            System.out.println("Storage is EMPTY!");
+            return;
+        }
+        FoodItem removed = queue.poll();
+        System.out.println("Removed: " + removed.name);
+    }
+
+    public void peekFood() {
+        if (queue.isEmpty()) {
+            System.out.println("Storage is EMPTY!");
+            return;
+        }
+        System.out.println("Next item to remove:");
+        queue.peek().display();
     }
 }
