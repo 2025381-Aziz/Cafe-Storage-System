3a6e8a7a65d7dc88fef5dcc8a5cd7de3e8225181
2025381-Aziz <2025381@student.cct.ie>
2026-04-19 19:20:26 +0100

    Implemented add food functionality with validation

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 package cafestoragesystem;
 
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,6 +28,56 @@ public class Main {
                 System.out.println("Invalid input. Please enter a number.");
                 continue;
             }
+            switch (choice) {
+
+    case 1:
+
+        System.out.print("Enter food name: ");
+        String name = sc.nextLine();
+
+        double weight = 0;
+        while (true) {
+            System.out.print("Enter weight (grams): ");
+            try {
+                weight = Double.parseDouble(sc.nextLine());
+                if (weight <= 0) {
+                    System.out.println("Weight must be greater than zero.");
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Invalid input. Enter numbers only.");
+            }
+        }
+
+        String date;
+        while (true) {
+            System.out.print("Enter best before date (YYYY-MM-DD): ");
+            date = sc.nextLine();
+
+            try {
+                LocalDate parsedDate = LocalDate.parse(date);
+                LocalDate today = LocalDate.now();
+                LocalDate maxDate = today.plusDays(14);
+
+                if (parsedDate.isBefore(today)) {
+                    System.out.println("Date cannot be in the past.");
+                } else if (parsedDate.isAfter(maxDate)) {
+                    System.out.println("Date must be within 2 weeks.");
+                } else {
+                    break;
+                }
+
+            } catch (DateTimeParseException e) {
+                System.out.println("Invalid format. Use YYYY-MM-DD.");
+            }
+        }
+
+        String time = java.time.LocalTime.now().toString();
+
+        FoodItem item = new FoodItem(name, (int) weight, date, time);
+        storage.addFood(item);
+        break;
         }
     }
 }
