60b02f14c0ccf23434f08415a98c81eedbe96f5c
2025381-Aziz <2025381@student.cct.ie>
2026-04-19 18:58:09 +0100

    Implemented addFood method

--- Storage.java
+++ Storage.java
@@ -6,4 +6,15 @@ class Storage {
 
     private Queue<FoodItem> queue = new LinkedList<>();
     private final int MAX_CAPACITY = 8;
+
+    public void addFood(FoodItem item) {
+
+        if (queue.size() >= MAX_CAPACITY) {
+            System.out.println("Storage is FULL (Max 8 items)");
+            return;
+        }
+
+        queue.add(item);
+        System.out.println("Food added successfully!");
+    }
 }
