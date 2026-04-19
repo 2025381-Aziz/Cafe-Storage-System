e47637f8d884ba0e7e711a862394119d8ed7babc
2025381-Aziz <2025381@student.cct.ie>
2026-04-19 19:17:47 +0100

    Added menu and input handling

--- Main.java
+++ Main.java
@@ -8,5 +8,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Storage storage = new Storage();
 
+        while (true) {
+
+            System.out.println("\n==== CAFE STORAGE MENU ====");
+            System.out.println("1. Add Food");
+            System.out.println("2. Remove Food");
+            System.out.println("3. Peek (View Next)");
+            System.out.println("4. Display All");
+            System.out.println("5. Search Food");
+            System.out.println("6. Exit");
+            System.out.print("Choose option: ");
+
+            int choice;
+            try {
+                choice = Integer.parseInt(sc.nextLine());
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter a number.");
+                continue;
+            }
+        }
     }
 }
