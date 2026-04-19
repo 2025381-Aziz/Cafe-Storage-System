29eb9873a32eb4572852896dfac418de5f9ab9b5
2025381-Aziz <2025381@student.cct.ie>
2026-04-19 18:51:40 +0100

    Added constructor to FoodItem class

--- FoodItem.java
+++ FoodItem.java
@@ -6,4 +6,11 @@ class FoodItem {
     double weight;
     String bestBefore;
     String timeAdded;
+
+    public FoodItem(String name, double weight, String bestBefore, String timeAdded) {
+        this.name = name;
+        this.weight = weight;
+        this.bestBefore = bestBefore;
+        this.timeAdded = timeAdded;
+    }
 }
